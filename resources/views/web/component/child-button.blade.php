de52dd739c60ffd286392fc7627d63cf4e72b79f
Gedion Daniel <gediondaniel321@gmail.com>
2025-09-04 22:08:18 +0200

    updated

--- resources/views/web/component/child-button.blade.php
+++ resources/views/web/component/child-button.blade.php
@@ -2,10 +2,14 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-8 offset-lg-2 section-title text-center mb-0">
-				<h3  data-aos="fade-up">{{ __('message.GetStartedFreeAccount') }}</h3>
+				<h3  data-aos="fade-up">Bring Your Child One Step closer To Home</h3>
 				<a data-aos="fade-up" href="{{ route('register') }}" class="btn btn-tra-grey lh-25">
-					START MY CHILD PETITION APPLICATION
+					Start Your Child’s Petition Today
 				</a>
+				<p>Still have a question?
+                            <a href="{{ route('contactUs') }}">
+                                Ask your question here
+                            </a></p>
 				<div data-aos="fade-up" class="account_bbl_div">
 					<a class="account_bbl_logo" target="_blank" href="{{ route('register') }}">
 						<img src="{{ asset('assets/img/BBB_A_Rating.png') }}" alt="">
