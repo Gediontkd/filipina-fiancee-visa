7ff54c24edfaa56d7e2d705e3fddc9aa0ab9dd54
Gedion Daniel <gediondaniel321@gmail.com>
2025-09-09 01:46:45 +0200

    updated

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\UserController as AdminUserController;
 use App\Http\Controllers\Admin\ApplicationController;
+use App\Http\Controllers\Admin\MonitoringController;
 
 /*
 |--------------------------------------------------------------------------
